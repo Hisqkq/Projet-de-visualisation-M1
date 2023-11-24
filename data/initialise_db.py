74b9eaba48c3e22243d101c33350a2f5cb67cd5d
theolavandier <120734251+Hisqkq@users.noreply.github.com>
2023-11-25 00:00:23 +0100

    Initialise national Data

--- data/initialise_db.py
+++ data/initialise_db.py
@@ -1,5 +1,15 @@
 import db_constructor
 
+print("Starting database construction...")
+
+#### Regional data ####
+print("Filling regional data...")
 db_constructor.create_collection("DonneesRegionales")
 db_constructor.update_data("eco2mix-regional-tr", "DonneesRegionales")
 db_constructor.update_data("eco2mix-regional-cons-def", "DonneesRegionales")
+
+#### National data ####
+print("Filling national data...")
+db_constructor.create_collection("DonneesNationales")
+db_constructor.update_data("eco2mix-national-tr", "DonneesNationales")
+db_constructor.update_data("eco2mix-national-cons-def", "DonneesNationales")
