b92a0a624dbbba20ff44bbb68ef0dc97805b2783
theolavandier <120734251+Hisqkq@users.noreply.github.com>
2023-11-24 23:39:36 +0100

    Use correct data

--- data/initialise_db.py
+++ data/initialise_db.py
@@ -2,5 +2,4 @@ import db_constructor
 
 db_constructor.create_collection("DonneesRegionales")
 db_constructor.update_data("eco2mix-regional-tr", "DonneesRegionales")
-db_constructor.create_collection("eco2mix_def")
-db_constructor.update_data("eco2mix-regional-cons-def", "eco2mix_def")
+db_constructor.update_data("eco2mix-regional-cons-def", "DonneesRegionales")
--- view/figures.py
+++ view/figures.py
@@ -3,11 +3,12 @@ import data.db_services as dbs
 
 
 #### data used by graphs ####
-prodution_nucleaire = dbs.get_data_group_by_sum("eco2mix", "date", ["consommation", "ech_physiques", "eolien", "hydraulique", "nucleaire"], 1)
-données_1mars2023 = dbs.get_data_from_one_date("eco2mix", "2023-03-01")
+
+prodution_nucleaire = dbs.get_data_group_by_sum("DonneesRegionales", "date", ["consommation", "ech_physiques", "eolien", "hydraulique", "nucleaire"], 1)
+données_1mars2023 = dbs.get_data_from_one_date("DonneesRegionales", "2022-09-12")
 
 
-
+#### graphs ####
 
 fig = px.line(prodution_nucleaire, x="_id", y="nucleaire")
 fig2 = px.area(données_1mars2023, x="date_heure", y="nucleaire", color="libelle_region")
