5dc485992dcd7926300190671281ebab75d6c9a6
HamadTria <triahamad@hotmail.com>
2023-12-06 19:51:23 +0100

    Add a year dropdown menu to datepicker and fix spelling in Readme file

--- view/datepicker.py
+++ view/datepicker.py
@@ -1,8 +1,11 @@
 from dash import dcc, html, callback
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import datetime
 
+year_options = [{'label': i, 'value': i} for i in range(2013, datetime.datetime.now().year + 1)]
+
 datepicker = html.Div([
+    dcc.Dropdown(id='year-dropdown', options=year_options, placeholder='Select Year'),
     dcc.DatePickerRange(
         id='date-picker',
         start_date='2020-01-01',
@@ -12,12 +15,26 @@ datepicker = html.Div([
         min_date_allowed= "2013-01-01",
     ),
     html.Button('Clear', id='clear-button', n_clicks=0, className='btn btn-primary'),
+    html.Div(id='n-clicks-store', style={'display': 'none'}),
 ])
 
 @callback(
     Output('date-picker', 'start_date'),
     Output('date-picker', 'end_date'),
+    Output('year-dropdown', 'value'),
+    Output('n-clicks-store', 'children'),
+    Input('year-dropdown', 'value'),
     Input('clear-button', 'n_clicks'),
+    State('n-clicks-store', 'children'),
 )
-def clear_dates(_):
-    return None, None
+def update_dates(year, n_clicks, n_clicks_store):
+    if n_clicks_store is None:
+        n_clicks_store = 0
+    if n_clicks > n_clicks_store:
+        return None, None, None, n_clicks
+    elif year:
+        start_date = datetime.date(year, 1, 1)
+        end_date = datetime.date(year, 1, 2)
+        return start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'), year, n_clicks
+    else:
+        return None, None, None, n_clicks
