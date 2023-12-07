a96c9b2b2f0a84b020cd5ec73ef376ec2165bf84
Louis Delignac <louis.dlgnc@gmail.com>
2023-12-07 12:03:31 +0100

    Modified layout in function
    Renamed zaccueil to accueil

--- pages/echanges.py
+++ pages/echanges.py
@@ -7,30 +7,31 @@ import view.map as map
 
 register_page(__name__)
 
-layout = dbc.Container([
-    dbc.NavbarSimple(
-        brand="Les échanges commerciaux aux frontières", 
-        color="primary", 
-        dark=True, 
-        className="mb-4"
-    ),
-    dbc.Row([
-        dbc.Col(dcc.Link(html.Button('Accueil', className='btn btn-primary'), href='/'), width=12),
-        dbc.Col(datepicker.datepicker, width=12),
-    ]),
-    dbc.Row([
-        dbc.Col(dcc.Graph(
-            id='choropleth-map', 
-            figure=map.build_metropolitan_map(),
-            config={'displayModeBar': False}
-        ), lg=6),
-        dbc.Col(dcc.Graph(
-            id="stacked_bar_chart_echanges", 
-            figure=figures.build_stacked_bar_chart(["ech_comm_angleterre", "ech_comm_espagne", "ech_comm_italie", "ech_comm_suisse"], "2020-01-01", "2020-01-02")
-        ), lg=6)
-    ]),
-    html.Footer(html.P("PVA - Louis Delignac & Théo Lavandier & Hamad Tria - CMI ISI - 2023", className="text-center"))
-], fluid=True)
+def layout():
+    return dbc.Container([
+        dbc.NavbarSimple(
+            brand="Les échanges commerciaux aux frontières", 
+            color="primary", 
+            dark=True, 
+            className="mb-4"
+        ),
+        dbc.Row([
+            dbc.Col(dcc.Link(html.Button('Accueil', className='btn btn-primary'), href='/'), width=12),
+            dbc.Col(datepicker.datepicker, width=12),
+        ]),
+        dbc.Row([
+            dbc.Col(dcc.Graph(
+                id='choropleth-map', 
+                figure=map.build_metropolitan_map(),
+                config={'displayModeBar': False}
+            ), lg=6),
+            dbc.Col(dcc.Graph(
+                id="stacked_bar_chart_echanges", 
+                figure=figures.build_stacked_bar_chart(["ech_comm_angleterre", "ech_comm_espagne", "ech_comm_italie", "ech_comm_suisse"], "2020-01-01", "2020-01-02")
+            ), lg=6)
+        ]),
+        html.Footer(html.P("PVA - Louis Delignac & Théo Lavandier & Hamad Tria - CMI ISI - 2023", className="text-center"))
+    ], fluid=True)
 
 
 @callback(
