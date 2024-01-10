0dbab246f8a25839f7f99db4b17e5b538255eafc
Louis Delignac <louis.dlgnc@gmail.com>
2024-01-10 23:18:36 +0100

    new page comparer

--- pages/production.py
+++ pages/production.py
@@ -1,5 +1,6 @@
 import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
+import urllib.parse
 from dash import register_page, html, dcc, callback, no_update
 from dash.dependencies import Input, Output, State
 
@@ -71,7 +72,58 @@ def layout():
                 config={'displayModeBar': False}
             )
         ]),
-            
+        dbc.Row([
+            dmc.Select(
+                placeholder="Choissez une région à comparer",
+                id="select-region-to-compare1",
+                data=[
+                    {'value': 'Auvergne-Rhône-Alpes', 'label': 'Auvergne-Rhône-Alpes'},
+                    {'value': 'Bourgogne-Franche-Comté', 'label': 'Bourgogne-Franche-Comté'},
+                    {'value': 'Bretagne', 'label': 'Bretagne'},
+                    {'value': 'Centre-Val de Loire', 'label': 'Centre-Val de Loire'},
+                    {'value': 'Corse', 'label': 'Corse'},
+                    {'value': 'Grand Est', 'label': 'Grand Est'},
+                    {'value': 'Hauts-de-France', 'label': 'Hauts-de-France'},
+                    {'value': 'Île-de-France', 'label': 'Île-de-France'},
+                    {'value': 'Normandie', 'label': 'Normandie'},
+                    {'value': 'Nouvelle-Aquitaine', 'label': 'Nouvelle-Aquitaine'},
+                    {'value': 'Occitanie', 'label': 'Occitanie'},
+                    {'value': 'Pays de la Loire', 'label': 'Pays de la Loire'},
+                    {'value': 'Provence-Alpes-Côte d\'Azur', 'label': 'Provence-Alpes-Côte d\'Azur'},
+                ], 
+                value='Nouvelle-Aquitaine',
+                style={"width": "25%"}
+            ),
+            dmc.Select(
+                placeholder="Choissez une région à comparer",
+                id="select-region-to-compare2",
+                data=[
+                    {'value': 'Auvergne-Rhône-Alpes', 'label': 'Auvergne-Rhône-Alpes'},
+                    {'value': 'Bourgogne-Franche-Comté', 'label': 'Bourgogne-Franche-Comté'},
+                    {'value': 'Bretagne', 'label': 'Bretagne'},
+                    {'value': 'Centre-Val de Loire', 'label': 'Centre-Val de Loire'},
+                    {'value': 'Corse', 'label': 'Corse'},
+                    {'value': 'Grand Est', 'label': 'Grand Est'},
+                    {'value': 'Hauts-de-France', 'label': 'Hauts-de-France'},
+                    {'value': 'Île-de-France', 'label': 'Île-de-France'},
+                    {'value': 'Normandie', 'label': 'Normandie'},
+                    {'value': 'Nouvelle-Aquitaine', 'label': 'Nouvelle-Aquitaine'},
+                    {'value': 'Occitanie', 'label': 'Occitanie'},
+                    {'value': 'Pays de la Loire', 'label': 'Pays de la Loire'},
+                    {'value': 'Provence-Alpes-Côte d\'Azur', 'label': 'Provence-Alpes-Côte d\'Azur'},
+                ], 
+                value='Occitanie',
+                style={"width": "25%"}
+            ),
+            dcc.Link(
+                id="comparer-button", 
+                children=[
+                    dbc.Button('Comparer', color="light", className="ms-auto"),
+                ],
+                href='/comparer/Nouvelle-Aquitaine-Alpes&Occitanie',
+                style={"width": "25%"}
+            )
+        ]),
         html.Footer(html.P("PVA - Louis Delignac & Théo Lavandier & Hamad Tria - CMI ISI - 2023", className="text-center"))
     ], fluid=True)
 
@@ -90,6 +142,25 @@ def update_map(selected_data, data):
         fig = map.build_region_map(selected_data['points'][0]['location'])
         return fig, selected_data['points'][0]['location']
     return france_map, "France"
+
+@callback(
+    Output('select-region-to-compare1', 'value'),
+    [Input('memory-output', 'data')]
+)
+def update_select_region_to_compare1(data):
+    """Update the first select region to compare."""
+    if data == "France":
+        return no_update
+    return data
+
+@callback(
+    Output('comparer-button', 'href'),
+    [Input('select-region-to-compare1', 'value'),
+     Input('select-region-to-compare2', 'value')]
+)
+def update_comparer_button(region1, region2):
+    """Update the comparer button."""
+    return f'/comparer/{urllib.parse.quote(region1)}&{urllib.parse.quote(region2)}'
 
 @callback(
     Output('production_pie_chart_by_sector', 'figure'),
