0b72cb90a4124f52565c719ba3d508521fc3220c
HamadTria <triahamad@hotmail.com>
2024-01-11 20:57:29 +0100

    Removing unused functions from db_services file

--- data/db_services.py
+++ data/db_services.py
@@ -68,71 +68,6 @@ def get_data(collection: str, unwind_sector=None, match_conditions=None, group_c
     return execute_aggregation(collection, pipeline)
 
 
-def get_data_group_by_sum(collection: str, group_sector: str, sum_sectors: [str], order: int) -> list:
-    """Enable User to get the sum of sectors from a collection, grouped by a specific sector.
-    
-    Parameters:
-    ----------
-    collection : str
-        Name of the collection.
-    group_sector : str
-        Sector to group by.
-    sum_sectors : list of str
-        Sectors to sum.
-    order : int
-        Sorting order (1 for ascending, -1 for descending).
-
-    Returns:
-    -------
-    list
-        List of aggregated documents.
-    """
-    group_conditions = {
-        "_id": f"$results.{group_sector}",
-        **{sector: {"$sum": f"$results.{sector}"} for sector in sum_sectors}
-    }
-    sort_conditions = {"_id": order}
-    return get_data(collection, 
-                    unwind_sector="$results", 
-                    group_conditions=group_conditions, 
-                    sort_conditions=sort_conditions)
-
-
-def get_data_from_one_date(collection: str, date: str, region: str = None) -> list:
-    """Enable User to get the data from a collection for a specific date.
-    
-    Parameters:
-    ----------
-    collection : str
-        Name of the collection.
-    date : str
-        Date for which to get the data.
-    region : str (optional)
-        Region for which to get the data.
-        
-    Returns:
-    -------
-    list
-        List of documents for the specified date.
-    """
-    if region:
-        match_conditions = {"results.date": date, "results.libelle_region": region}
-        project_conditions = {"_id": 0, "date": "$results.date", "data": "$results.data"}
-        replace_root_conditions = None
-        sort_conditions={"results.date_heure": 1}
-    else:
-        match_conditions = {"results.date": date}
-        project_conditions = None
-        replace_root_conditions = "$results"
-        sort_conditions={"date_heure": 1}
-    return get_data(collection, 
-                    unwind_sector="$results", 
-                    match_conditions=match_conditions, 
-                    project_conditions=project_conditions, 
-                    replace_root_conditions=replace_root_conditions, 
-                    sort_conditions=sort_conditions)
-
-
 def get_data_from_one_date_to_another_date(collection: str, date1: str, date2: str, region: str = None) -> list:
     """Get data from a collection for a specific date range using the modular get_data function.
     
@@ -207,92 +142,6 @@ def get_mean_for_sectors(collection: str, date1: str, date2: str, mean_sectors: 
                     group_conditions=group_conditions, 
                     project_conditions=project_conditions,
                     sort_conditions={"_id": 1})
-
-
-def get_average_values(collection: str, sectors: [str]) -> dict:
-    """Enable User to get the averages of many sectors (when values are not null) from a collection.
-    
-    Parameters:
-    ----------
-    collection : str
-        Name of the collection.
-    sectors : list of str
-        Sectors to calculate the average.
-        
-    Returns:
-    -------
-    dict
-        Dictionary of averages for each sector.
-    """
-    match_conditions = {"$and": [{f"results.{sector}": {"$ne": None, "$exists": True, "$type": ["double", "int", "long", "decimal"]}} for sector in sectors]}
-    group_conditions = {
-        "_id": None, 
-        **{f"avg_{sector}": {"$avg": f"$results.{sector}"} for sector in sectors}
-    }
-    project_conditions = {"_id": 0, **{f"avg_{sector}": 1 for sector in sectors}}
-    result = get_data(collection, 
-                    unwind_sector="$results", 
-                    match_conditions=match_conditions, 
-                    group_conditions=group_conditions,
-                    project_conditions=project_conditions)
-    return result[0] if result else {}
-
-
-def get_max_value(collection: str, sector: str, columns: [str]) -> dict:
-    """Enable User to get the max value of a sector and its corresponding data (columns) from a collection.
-
-    Parameters:
-    ----------
-    collection : str
-        Name of the collection.
-    sector : str
-        Sector to check.
-    columns : list of str
-        Columns to return.
-
-    Returns:
-    -------
-    dict
-        Dictionary of the max value and its corresponding data.
-    """
-    match_conditions = {f"results.{sector}": {"$ne": None, "$exists": True, "$type": ["double", "int", "long", "decimal"]}}
-    sort_conditions = {f"results.{sector}": -1}
-    project_conditions = {"_id": 0, **{f"results.{column}": 1 for column in columns}}
-    result = get_data(collection, 
-                    unwind_sector="$results", 
-                    match_conditions=match_conditions, 
-                    sort_conditions=sort_conditions,
-                    project_conditions=project_conditions)
-    return result[0] if result else {}
-
-
-def get_sum_values(collection: str, sectors: [str]) -> dict:
-    """Enable User to get the sum of many sectors (when values are not null) from a collection.
-    
-    Parameters:
-    ----------
-    collection : str
-        Name of the collection.
-    sectors : list of str
-        Sectors to calculate the sum.
-        
-    Returns:
-    -------
-    dict
-        Dictionary of sums for each sector.
-    """
-    match_conditions = {"$and": [{f"results.{sector}": {"$ne": None, "$exists": True, "$type": ["double", "int", "long", "decimal"]}} for sector in sectors]}
-    group_conditions = {
-        "_id": None, 
-        **{f"sum_{sector}": {"$sum": f"$results.{sector}"} for sector in sectors}
-    }
-    project_conditions = {"_id": 0, **{f"sum_{sector}": 1 for sector in sectors}}
-    result = get_data(collection, 
-                    unwind_sector="$results", 
-                    match_conditions=match_conditions, 
-                    group_conditions=group_conditions,
-                    project_conditions=project_conditions)
-    return result[0] if result else {}
 
 
 def transform_data_to_df(data: list) -> pd.DataFrame:
@@ -416,5 +265,3 @@ def get_mean_consommation_by_region(collection: str, date1: str, date2: str) -> 
     mean_cons_dict = {d['region']: d['mean_consommation'] for d in mean_cons_formatted}
     
     return mean_cons_dict
-    
-    
